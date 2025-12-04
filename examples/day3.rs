2543781279bb4f970cf897eb36fee5140cda2946
Maximilian Arnold <arnoldma80620@th-nuernberg.de>
2025-12-04 01:32:23 +0100

    day3_2

--- examples/day3.rs
+++ examples/day3.rs
@@ -4,8 +4,8 @@ fn main() {
     const FILE_PATH: &str = "input/day3.txt";
     let contents = fs::read_to_string(FILE_PATH).unwrap();
 
-    let joltage: u32 = contents
-        .split("\r\n")
+    let joltage: u64 = contents
+        .lines()
         .map(|line| line.bytes().map(|b| b - b'0'))
         .map(extract_joltage)
         .sum();
@@ -13,27 +13,34 @@ fn main() {
     println!("joltage: {joltage}")
 }
 
-fn extract_joltage(seq: impl Iterator<Item = u8>) -> u32 {
+const STRIP_LEN: usize = 12;
+fn extract_joltage(mut seq: impl Iterator<Item = u8>) -> u64 {
+    let mut strip: Vec<_> = seq.by_ref().take(STRIP_LEN).collect();
     let seq: Box<_> = seq.collect();
 
-    let mut l = seq[0];
-    let mut r = seq[1];
-
-    for i in 2..seq.len() {
+    'outer: for i in 0..seq.len() {
         let val = seq[i];
 
-        if r > l {
-            l = r;
-            r = val;
-            continue;
+        for i in 0..STRIP_LEN - 1 {
+            if strip[i] < strip[i + 1] {
+                strip.remove(i);
+                strip.push(val);
+                continue 'outer;
+            }
         }
 
-        if val > r {
-            r = val;
+        let last = strip.last_mut().unwrap();
+        if val > *last {
+            *last = val;
         }
     }
 
-    (l * 10 + r) as u32
+    strip
+        .into_iter()
+        .rev()
+        .map(|x| x as u64)
+        .fold((0, 1), |(out, mul), x| (out + mul * x, mul * 10))
+        .0
 }
 
 #[cfg(test)]
@@ -41,15 +48,11 @@ mod tests {
     pub use super::*;
     use test_case::test_case;
 
-    #[test_case("987654321111111", 98)]
-    #[test_case("811111111111119", 89)]
-    #[test_case("234234234234278", 78)]
-    #[test_case("818181911112111", 92)]
-    #[test_case("892", 92)]
-    #[test_case("1234", 34)]
-    #[test_case("9811", 98)]
-    #[test_case("9217", 97)]
-    fn test_extract_joltage(seq: &'static str, expected: u32) {
+    #[test_case("987654321111111", 987654321111)]
+    #[test_case("811111111111119", 811111111119)]
+    #[test_case("234234234234278", 434234234278)]
+    #[test_case("818181911112111", 888911112111)]
+    fn test_extract_joltage(seq: &'static str, expected: u64) {
         let got = extract_joltage(seq.bytes().map(|b| b - b'0'));
         assert_eq!(got, expected);
     }
