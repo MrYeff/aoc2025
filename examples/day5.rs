059940f15520b2a3611955d4b9598a6ec95ca66a
Maximilian Arnold <arnoldma80620@th-nuernberg.de>
2025-12-06 02:58:08 +0100

    day5_2

--- examples/day5.rs
+++ examples/day5.rs
@@ -4,7 +4,8 @@ fn main() {
     const FILE_PATH: &str = "input/day5.txt";
     let contents = fs::read_to_string(FILE_PATH).unwrap();
     let mut lines = contents.lines();
-    let ranges: Vec<(u64, u64)> = lines
+
+    let mut ranges: Vec<(u64, u64)> = lines
         .by_ref()
         .take_while(|line| !line.is_empty())
         .map(|line| {
@@ -13,14 +14,29 @@ fn main() {
         })
         .collect();
 
-    let mut fresh = 0;
-    for id in lines {
-        let id: u64 = id.parse().unwrap();
-        if ranges.iter().any(|(l, u)| id >= *l && id <= *u) {
-            fresh += 1;
+    ranges.sort_by_key(|r| r.0);
+
+    let mut count = 0;
+    let mut prev_u = 0;
+    for range in ranges.into_iter() {
+        if prev_u >= range.1 {
+            // completly inside prev
+            continue;
         }
+
+        count += range.1 - range.0;
+
+        if prev_u >= range.0 {
+            // overlapping ranges
+            count -= prev_u - range.0
+        } else {
+            // count each element not length only 3-5 = 3 (3,4,5)
+            count += 1
+        }
+
+        prev_u = range.1;
     }
-    println!("Fresh IDs: {}", fresh);
+    println!("count {count}")
 }
 
 #[cfg(test)]
